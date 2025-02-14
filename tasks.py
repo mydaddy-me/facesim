3fd983a5fc173f261db7433260986ac38d155ad6
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-02-14 08:34:13 +0200

    fixed taskfile

--- tasks.py
+++ tasks.py
@@ -4,18 +4,22 @@ from pathlib import Path
 
 app = typer.Typer()
 
-MIN_DIM = 350
+MIN_DIM = 300
 def del_if_small(file:Path):
     img = cv2.imread(str(file))
     h, w = img.shape[:2]
     if h < MIN_DIM or w < MIN_DIM:
-        print(f"Deleting {file} because it's too small")
+        print(f'Deleting {file} {h}x{w}')
         file.unlink()
 
 @app.command()
 def del_small_files():
     for file in Path('data').rglob('*.jpg'):
         del_if_small(file)
+
+@app.command()
+def nop():
+    pass
 
 if __name__ == "__main__":
     app()
