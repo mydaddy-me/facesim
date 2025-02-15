4a2e5f2b32cec9595eee26c7070ad5a1f7b7e082
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-02-15 14:55:57 +0200

    train

--- facesim/train.py
+++ facesim/train.py
@@ -5,8 +5,6 @@ import torch
 from lightning import seed_everything
 from lightning.pytorch.callbacks import ModelCheckpoint
 from torch.nn.functional import cosine_similarity as cos
-from torch.nn.functional import cross_entropy, softmax
-from torchmetrics.functional import accuracy
 
 from facesim.model import FaceSim, Parts
 
@@ -28,29 +26,31 @@ class FaceSimModule(pl.LightningModule):
 
         self.log('sim/ap', ap_sim.mean(), prog_bar=True)
 
-        sim = torch.stack([ap_sim, an_sim], dim=1)
+        return -ap_sim.mean()
 
-        assert sim.shape == (len(a), 2), \
-            f"Expected ({len(a)}, 2) got {sim.shape}"
+        # sim = torch.stack([ap_sim, an_sim], dim=1)
 
-        prob = softmax(sim, dim=1)
-        self.log(
-            f'acc/{name}',
-            accuracy(
-                prob[:, 0],
-                torch.ones_like(ap_sim),
-                task='binary'),
-            prog_bar=True)
+        # assert sim.shape == (len(a), 2), \
+        #     f"Expected ({len(a)}, 2) got {sim.shape}"
 
-        loss = cross_entropy(
-            sim,
-            torch.zeros_like(
-                ap_sim,
-                dtype=torch.long))
+        # prob = softmax(sim, dim=1)
+        # self.log(
+        #     f'acc/{name}',
+        #     accuracy(
+        #         prob[:, 0],
+        #         torch.ones_like(ap_sim),
+        #         task='binary'),
+        #     prog_bar=True)
 
-        self.log(f'loss/{name}', loss, prog_bar=True)
+        # loss = cross_entropy(
+        #     sim,
+        #     torch.zeros_like(
+        #         ap_sim,
+        #         dtype=torch.long))
 
-        return loss
+        # self.log(f'loss/{name}', loss, prog_bar=True)
+
+        # return loss
 
     def training_step(self, batch, batch_idx):
         return self.step(batch, 'train')
