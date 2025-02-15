8c37927525195a37acc361cccc3b96bd40d2058e
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-02-15 09:58:47 +0200

    fixed taskfile

--- tasks.py
+++ tasks.py
@@ -10,6 +10,8 @@ app = typer.Typer()
 class fs:
     data = Path('data')
     faces = Path('faces')
+    eyebrow = Path('eyebrow')
+    eye = Path('eye')
     nose = Path('nose')
     lips = Path('lips')
 
@@ -36,8 +38,11 @@ def faces():
 
 @app.command()
 def parts():
+    fs.eyebrow.mkdir(exist_ok=True)
+    fs.eye.mkdir(exist_ok=True)
     fs.nose.mkdir(exist_ok=True)
     fs.lips.mkdir(exist_ok=True)
+
 
     for f in tqdm(fs.faces.rglob('*.jpg')):
         label = f.parent.name
@@ -45,6 +50,35 @@ def parts():
 
         parts = crop_parts(face)
         
+        if parts.left_eyebrow is not None:
+            left_eyebrow_dir = fs.faces / label
+            left_eyebrow_dir.mkdir(exist_ok=True)
+            left_eyebrow_file = left_eyebrow_dir / f.name
+
+            cv2.imwrite(str(left_eyebrow_file), parts.left_eyebrow)
+
+    
+        if parts.right_eyebrow is not None:
+            right_eyebrow_dir = fs.faces / label
+            right_eyebrow_dir.mkdir(exist_ok=True)
+            right_eyebrow_file = right_eyebrow_dir / f.name
+
+            cv2.imwrite(str(right_eyebrow_file), parts.right_eyebrow)
+
+        if parts.left_eye is not None:
+            left_eye_dir = fs.faces / label
+            left_eye_dir.mkdir(exist_ok=True)
+            left_eye_file = left_eye_dir / f.name
+
+            cv2.imwrite(str(left_eye_file), parts.left_eye)
+
+        if parts.right_eye is not None:
+            right_eye_dir = fs.faces / label
+            right_eye_dir.mkdir(exist_ok=True)
+            right_eye_file = right_eye_dir / f.name
+
+            cv2.imwrite(str(right_eye_file), parts.right_eye)
+
         if parts.nose is not None:
             nose_dir = fs.nose / label
             nose_dir.mkdir(exist_ok=True)
@@ -58,6 +92,8 @@ def parts():
             lips_file = lips_dir / f.name
 
             cv2.imwrite(str(lips_file), parts.lips)
+
+        
 
 @app.command()
 def nop():
