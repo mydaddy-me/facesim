bc8cb3254113c4b09a1636ef8f551cb364f8700c
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-02-15 09:34:20 +0200

    fixed taskfile

--- tasks.py
+++ tasks.py
@@ -3,18 +3,19 @@ import cv2
 from tqdm import tqdm
 import typer
 
-from facesim import crop_face
+from facesim import crop_face, crop_parts
 
 app = typer.Typer()
 
 class fs:
     data = Path('data')
     faces = Path('faces')
+    nose = Path('nose')
 
 @app.command()
 def faces():
     fs.faces.mkdir(exist_ok=True)
-    for f in tqdm(Path('data').rglob('*.jpg')):
+    for f in tqdm(fs.data.rglob('*.jpg')):
         label = f.parent.name
         image = cv2.imread(str(f))
         face = crop_face(image)
@@ -31,6 +32,21 @@ def faces():
         out_dir.mkdir(exist_ok=True)
         out_file = out_dir / f.name
         cv2.imwrite(str(out_file), face)
+
+@app.command()
+def parts():
+    fs.nose.mkdir(exist_ok=True)
+    for f in tqdm(fs.faces.rglob('*.jpg')):
+        label = f.parent.name
+        face = cv2.imread(str(f))
+
+        parts = crop_parts(face)
+
+        out_dir = fs.nose / label
+        out_dir.mkdir(exist_ok=True)
+        out_file = out_dir / f.name
+
+        cv2.imwrite(str(out_file), parts.nose)
 
 @app.command()
 def nop():
